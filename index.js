61b35f3c2a78a4a275fb19e4ff93e01610a086fc
GersEsc <160088784+GersEsc@users.noreply.github.com>
2025-06-02 22:38:08 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 let estudiantes = [
   { id: 1, nombre: 'Luis', nota: 85 },
